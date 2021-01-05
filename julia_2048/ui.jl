ff8c8bca3fea9cea5a4a4a1ff36197aee419b5f7
Sam Fuchs <samcfuchs@gmail.com>
2021-01-05 13:52:29 -0500

    Add UI framework

--- julia_2048/ui.jl
+++ julia_2048/ui.jl
@@ -1,8 +1,32 @@
+include("game.jl")
+using .TwentyFortyEight
 using Blink
 
 function init()
     w = Window()
     load!(w, "style.css")
+    load!(w, "index.html")
+    load!(w, "index.js")
+
+    game = TwentyFortyEight.init()
+    display(game.board)
+
+    return game, w
 end
 
-init()
+function show(game)
+end
+
+function loop!(game, move)
+    res = TwentyFortyEight.turn!(game, move)
+    show(game)
+    if res == 1
+        game_over!(w)
+    end
+end
+
+function game_over!(w)
+    # Display message
+end
+
+# Javascript should call loop! with the move
